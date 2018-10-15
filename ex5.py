edf784c016080f12649893404bb44e5fc9e6b78b
annkon22 <43612983+annkon22@users.noreply.github.com>
2018-10-15 18:37:34 +0200

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,15 @@
+
+def fibo(n, a = 0, b = 1, fib = []):
+    if b < n:
+        fib.append(b)
+        a, b = b, a + b
+    else:
+        return fib
+    return fibo(n, a, b, fib)
+
+print(fibo(20)) 
+
+  
+
+
+
